237dd18676e615716e460ce6b6307e97fb6eaaf6
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-24 16:35:13 -0800

    finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,42 @@ module.exports = {
   get,
 };
 
+
 function success(item) {
+  if (item.enhancement !== 20) {
+    item.enhancement++;
+  }
   return { ...item };
 }
+
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement >= 15) {
+    item.durability -= 10;
+  } else {
+    item.durability--;
+  }
   return { ...item };
 }
+
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
+
 function get(item) {
-  return { ...item };
+  if(item.name.charAt(0) === '[') {
+    item.name = item.name.slice((item.name.indexOf(']')) + 2)
+  }
+
+  if (item.enhancement === 0) {
+    return { ...item };
+  } else {
+    return { ...item, name: `[+${item.enhancement}] ${item.name}`};
+  }
+  
 }
